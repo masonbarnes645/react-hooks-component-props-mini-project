ee8f78ad9dea6b5be458daf625d60aa4ae874794
Mason Barnes <barnesmason980@gmail.com>
2024-06-25 18:19:48 -0700

    lab complete

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import blogData from "../data/blog";
+
+
+function Header({ name }){
+    return(
+        <Header>
+        <h1>{ name }</h1>
+        </Header>
+    )
+}
+
+export default Header
